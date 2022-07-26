26713f91e78cadbc652a6bc0e75d3cec47fd0578
Crhistian Alexander Garcia Urbano <42595236+crhistian2296@users.noreply.github.com>
2022-07-26 13:35:59 +0200

    Markers, SearchBar and Hooks

--- ex2/src/App.jsx
+++ ex2/src/App.jsx
@@ -4,22 +4,37 @@ import {
   Container,
   Flex,
   HStack,
-  IconButton,
   Input,
   Skeleton,
   Stack,
 } from "@chakra-ui/react";
 import { faLocationCrosshairs } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
+import { useState } from "react";
+import useForm from "./hooks/useForm";
 
 // const key = import.meta.env.VITE_APP_GOOGLE_MAPS_API_KEY;
 
-const center = { lat: 48.8584, lng: 2.2945 };
+// madrid coordinates
+const center = { lat: 40.4168, lng: -3.7038 };
 
 // const libraries = ["places"];
 
 function App() {
+  const [markers, setMarkers] = useState([]);
+  const { formValues, handleInputChange, reset } = useForm({
+    location: "",
+  });
+
+  const { location } = formValues;
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    return formValues.location;
+    // reset();
+  };
+
   const { isLoaded } = useJsApiLoader({
     googleMapsApiKey: import.meta.env.VITE_APP_GOOGLE_MAPS_API_KEY,
     // libraries,
@@ -49,7 +64,20 @@ function App() {
           center={center}
           zoom={15}
           mapContainerStyle={{ width: "99vw", height: "98vh" }}
-        ></GoogleMap>
+          onClick={e =>
+            setMarkers(initialState => [
+              ...initialState,
+              { lat: e.latLng.lat(), lng: e.latLng.lng(), time: new Date() },
+            ])
+          }
+        >
+          {markers.map(marker => (
+            <Marker
+              key={marker.time.toISOString()}
+              position={{ lat: marker.lat, lng: marker.lng }}
+            />
+          ))}
+        </GoogleMap>
       </Box>
       <Box
         p={4}
@@ -60,15 +88,31 @@ function App() {
         minW={Container.md}
       >
         <Flex flexDirection='row' alignItems='center'>
-          <HStack spacing={4}>
-            <Input type='text' zIndex={1} placeholder='type a location' />
-            <Button type='submit' color='black'>
+          <HStack spacing={4} h='40px'>
+            <Input
+              type='text'
+              h='28px'
+              zIndex={1}
+              placeholder='type a location'
+              name='location'
+              value={location}
+              onChange={handleInputChange}
+            />
+            <Button
+              type='submit'
+              colorScheme='whiteAlpha'
+              color='black'
+              h='34px'
+              onSubmit={() => {
+                handleSubmit();
+                reset();
+              }}
+            >
               Search
             </Button>
-            <IconButton
-              icon={<FontAwesomeIcon icon={faLocationCrosshairs} />}
-              p={2}
-            ></IconButton>
+            <Button p={8}>
+              <FontAwesomeIcon icon={faLocationCrosshairs} />
+            </Button>
           </HStack>
         </Flex>
       </Box>
